af3d03416023b3b3502f9e59f71365804869eefc
SSSH <sunshihao945@outlook.com>
2024-04-18 09:40:27 +0800

    fix: 打版环境需要构建

--- src/layout/plugin/business/params/index.ts
+++ src/layout/plugin/business/params/index.ts
@@ -2,6 +2,12 @@ const _headers = {
   Authorization: 'Bearer 2b6c0dd1831842a18ec8bdc16d2b793c'
 };
 
-export const headers = import.meta.env.DEV ? _headers : {};
+const pro_url = 'http://122.51.179.83:9000'
 
-export const base_path = import.meta.env.DEV ? 'apiAmis' : '';
+// export const headers = import.meta.env.DEV ? _headers : {};
+export const headers = {};
+
+
+// export const base_path = import.meta.env.DEV ? 'apiAmis' : pro_url;
+export const base_path = pro_url;
+
